f46682e252f8e04a1909e7b46c79c37db6ecbd84
bahelms <jimbonkgit@gmail.com>
2014-01-20 07:42:04 -0500

    added variables and assignment/conditional statements

--- simple_semantics.rb
+++ simple_semantics.rb
@@ -1,41 +1,53 @@
 # Operational semantics
 # Small-step semantics describing the AST of the Simple language
 
-class Machine < Struct.new(:expression)
+class Machine < Struct.new(:statement, :environment)
   def step
-    self.expression = expression.reduce
+    self.statement, self.environment = statement.reduce(environment)
   end
 
   def run
-    while expression.reducible?
-      puts expression
+    while statement.reducible?
+      puts "#{statement}, #{environment}"
       step
     end
-    puts expression
+    puts "#{statement}, #{environment}"
   end
 end
 
-class Value < Struct.new(:value)
-  def to_s
-    value.to_s
-  end
-
+module Inspect
   def inspect
     "<<#{self}>>"
   end
-
-  def reducible?
-    false
-  end
 end
 
-class Operation < Struct.new(:left, :right)
+class Variable < Struct.new(:name)
+  include Inspect 
+
+  def to_s
+    name.to_s
+  end
+
   def reducible?
     true
   end
 
-  def inspect
-    "<<#{self}>>"
+  def reduce(environment)
+    environment[name]
+  end
+end
+
+# Values
+
+class Value < Struct.new(:value)
+  include Inspect 
+
+  def to_s
+    value.to_s
+  end
+
+  def reducible?
+    false
   end
 end
 
@@ -45,50 +57,123 @@ end
 class Boolean < Value
 end
 
-class Add < Operation
+# Expressions
+
+class Expression < Struct.new(:left, :right)
+  include Inspect 
+
+  def reducible?
+    true
+  end
+end
+
+class Add < Expression
   def to_s
     "#{left} + #{right}"
   end
 
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      Add.new(left.reduce, right)
+      Add.new(left.reduce(environment), right)
     elsif right.reducible?
-      Add.new(left, right.reduce)
+      Add.new(left, right.reduce(environment))
     else
       Number.new(left.value + right.value)
     end
   end
 end
 
-class Multiply < Operation
+class Multiply < Expression
   def to_s
     "#{left} * #{right}"
   end
 
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      Multiply.new(left.reduce, right)
+      Multiply.new(left.reduce(environment), right)
     elsif right.reducible?
-      Multiply.new(left, right.reduce)
+      Multiply.new(left, right.reduce(environment))
     else
       Number.new(left.value * right.value)
     end
   end
 end
 
-class LessThan < Operation
+class LessThan < Expression
   def to_s
     "#{left} < #{right}"
   end
 
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      LessThan.new(left.reduce, right)
+      LessThan.new(left.reduce(environment), right)
     elsif right.reducible?
-      LessThan.new(left, right.reduce)
+      LessThan.new(left, right.reduce(environment))
     else
       Boolean.new(left.value < right.value)
+    end
+  end
+end
+
+# Statements
+
+class DoNothing
+  include Inspect
+
+  def to_s
+    "do-nothing"
+  end
+
+  def ==(other_statement)
+    other_statement.instance_of? DoNothing
+  end
+
+  def reducible?
+    false
+  end
+end
+
+class Assign < Struct.new(:name, :expression)
+  include Inspect
+
+  def to_s
+    "#{name} = #{expression}"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    if expression.reducible?
+      [Assign.new(name, expression.reduce(environment)), environment]
+    else
+      [DoNothing.new, environment.merge({ name => expression })]
+    end
+  end
+end
+
+class If < Struct.new(:condition, :consequence, :alternative)
+  include Inspect
+
+  def to_s
+    "if (#{condition}) { #{consequence} } else { #{alternative} }"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    if condition.reducible?
+      [If.new(condition.reduce(environment), consequence, alternative), environment]
+    else
+      case condition
+      when Boolean.new(true)
+        [consequence, environment]
+      when Boolean.new(false)
+        [alternative, environment]
+      end
     end
   end
 end
