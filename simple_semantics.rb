49c4201ce8bda9188dff4804d1ad13d7e3efcca3
bahelms <jimbonkgit@gmail.com>
2014-01-23 17:55:29 -0500

    finishes denotational semantics for statements

--- simple_semantics.rb
+++ simple_semantics.rb
@@ -179,6 +179,10 @@ class DoNothing
   def evaluate(environment)
     environment
   end
+
+  def to_ruby
+    "-> e { e }"
+  end
 end
 
 class Assign < Struct.new(:name, :expression)
@@ -199,6 +203,10 @@ class Assign < Struct.new(:name, :expression)
 
   def evaluate(environment)
     environment.merge({ name => expression.evaluate(environment) })
+  end
+
+  def to_ruby
+    "-> e { e.merge({ #{name.inspect} => (#{expression.to_ruby}).call(e) }) }"
   end
 end
 
@@ -231,6 +239,13 @@ class If < Struct.new(:condition, :consequence, :alternative)
       alternative.evaluate(environment)
     end
   end
+
+  def to_ruby
+    "-> e { if (#{condition.to_ruby}).call(e)" +
+      " then (#{consequence.to_ruby}).call(e)" +
+      " else (#{alternative.to_ruby}).call(e)" +
+      " end }"
+  end
 end
 
 class Sequence < Struct.new(:first, :second)
@@ -254,6 +269,10 @@ class Sequence < Struct.new(:first, :second)
   def evaluate(environment)
     second.evaluate(first.evaluate(environment))
   end
+
+  def to_ruby
+    "-> e { (#{second.to_ruby}).call((#{first.to_ruby}).call(e)) }"
+  end
 end
 
 class While < Struct.new(:condition, :body)
@@ -275,5 +294,12 @@ class While < Struct.new(:condition, :body)
     when Boolean.new(false)
       environment
     end
+  end
+
+  def to_ruby
+    "-> e {" +
+      " while (#{condition.to_ruby}).call(e); e = (#{body.to_ruby}).call(e); end;" +
+      " e" +
+      " }"
   end
 end
